be8d0df6d8665ef2b75e9ba7e3ff9386a1b69edd
heidless <rob.lockhart@yahoo.co.uk>
2024-02-29 10:12:12 +0000

    done

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,11 +1,11 @@
 import classes from './Modal.module.css';
 
-function Modal(props) {
+function Modal({children, onClose}) {
     return (
         <>
-            <div className={classes.backdrop}/>
+            <div className={classes.backdrop} onClick={onClose}/>
             <dialog open className={classes.modal}>
-                {props.children}
+                {children}
             </dialog>
         </>
     );
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -7,8 +7,13 @@ import Modal from './Modal.jsx'
 import classes from './PostsList.module.css'
 
 function PostsList(props) {
+    const [ modalIsVisible, setModalIsVisible ] = useState(true);
     const [ enteredBody, setEnteredBody ] = useState('');
     const [ enteredAuthor, setEnteredAuthor ] = useState('');
+
+    function hideModalHandler() {
+        setModalIsVisible(false);
+    }
 
     function bodyChangeHandler(event) {
         setEnteredBody(event.target.value);
@@ -18,14 +23,21 @@ function PostsList(props) {
         setEnteredAuthor(event.target.value);
     }
 
+    let modalContent;
+
+    if (modalIsVisible) {
+        modalContent = <Modal onClose={hideModalHandler}>
+        <NewPost 
+            onAuthorChange={authorChangeHandler} 
+            onBodyChange={bodyChangeHandler}
+        />
+    </Modal> 
+    }
+
+
     return (
         <>
-            <Modal>
-                <NewPost 
-                    onAuthorChange={authorChangeHandler} 
-                    onBodyChange={bodyChangeHandler}
-                />
-            </Modal>
+            {modalContent}
             <ul className={classes.posts}>       
                 <Post author={enteredAuthor} body={enteredBody} />
                 <Post author='Max' body='test body' />
